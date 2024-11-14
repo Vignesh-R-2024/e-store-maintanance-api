d0b6b6a07121e435d33c67970643191c1334fa9e
Vignesh-R-2024 <Vigneshwaran.r@outlook.in>
2024-11-14 15:15:39 +0530

    Added AutoMapper.

--- src/e-store-maintanance-api/Controllers/EStoreMaintananceController.cs
+++ src/e-store-maintanance-api/Controllers/EStoreMaintananceController.cs
@@ -5,6 +5,7 @@ using EStoreMaintanance.Model;
 using System.Collections.Generic;
 using EStoreMaintanance.Contract;
 using System.Linq;
+using AutoMapper;
 
 namespace EStoreMaintanance.api.Controllers;
 
@@ -13,43 +14,51 @@ namespace EStoreMaintanance.api.Controllers;
 
 public class EStoreMaintanance : ControllerBase
 {
-   ProductRepository productRepository = new ProductRepository("server=127.0.0.1;port=3306;database=e_store_maintanance;user=vicky;password=Dev2024");
-
-   private ProductServiceContract Convert(Product product)
+   private ProductRepository _productRepository;
+   private IMapper _mapper;
+   public EStoreMaintanance(IMapper mapper)
    {
-      ProductServiceContract productServiceContract = new ProductServiceContract();
+      _mapper = mapper;
+      _productRepository = new ProductRepository("server=127.0.0.1;port=3306;database=e_store_maintanance;user=vicky;password=Dev2024"); 
+   }
+  
+   // private ProductServiceContract Convert(Product product)
+   // {
+   //    ProductServiceContract productServiceContract = new ProductServiceContract();
 
-      productServiceContract.Id = product.product_id;
-      productServiceContract.ProductName = product.product_name;
-      productServiceContract.ProductType = product.product_type;
-      productServiceContract.SupplierId =  product.supplier_id;
-      productServiceContract.UnitsinStock = product.units_in_stock;
-      productServiceContract.UnitsonOrder = product.units_on_order;
-      productServiceContract.QuantityPerUnit = product.Quantity_per_unit;
-      productServiceContract.Price = product.Price;
-      return productServiceContract;
-   }
-   private Product ConvertProduct(ProductServiceContract productServiceContract)
-   {
-      Product product = new Product();
-      product.product_id = productServiceContract.Id ;
-      product.product_name = productServiceContract.ProductName;
-      product.product_type = productServiceContract.ProductType;
-      product.supplier_id = productServiceContract.SupplierId;
-      product.units_in_stock = productServiceContract.UnitsinStock;
-      product.units_on_order = productServiceContract.UnitsonOrder;
-      product.Quantity_per_unit = productServiceContract.QuantityPerUnit;
-      product.Price = productServiceContract.Price;
-      return product;
-   }
+   //    productServiceContract.Id = product.product_id;
+   //    productServiceContract.ProductName = product.product_name;
+   //    productServiceContract.ProductType = product.product_type;
+   //    productServiceContract.SupplierId =  product.supplier_id;
+   //    productServiceContract.UnitsinStock = product.units_in_stock;
+   //    productServiceContract.UnitsonOrder = product.units_on_order;
+   //    productServiceContract.QuantityPerUnit = product.Quantity_per_unit;
+   //    productServiceContract.Price = product.Price;
+   //    return productServiceContract;
+   // }
+   // private Product ConvertProduct(ProductServiceContract productServiceContract)
+   // {
+   //    Product product = new Product();
+   //    product.product_id = productServiceContract.Id ;
+   //    product.product_name = productServiceContract.ProductName;
+   //    product.product_type = productServiceContract.ProductType;
+   //    product.supplier_id = productServiceContract.SupplierId;
+   //    product.units_in_stock = productServiceContract.UnitsinStock;
+   //    product.units_on_order = productServiceContract.UnitsonOrder;
+   //    product.Quantity_per_unit = productServiceContract.QuantityPerUnit;
+   //    product.Price = productServiceContract.Price;
+   //    return product;
+   // }
+
+
    
    [HttpGet("{id}")]
    public IActionResult GetProduct(int id)
    {
-     var product = productRepository.GetProduct(id);
+     var product = _productRepository.GetProduct(id);
      if(product is null)
       return NotFound($"Product Id: {id} Is Not Found.");
-     var productServiceContract = Convert(product);
+     var productServiceContract = _mapper.Map<ProductServiceContract>(product);
      return Ok(productServiceContract);
    }
 
@@ -57,13 +66,13 @@ public class EStoreMaintanance : ControllerBase
    public IActionResult GetProductListOfSupplierId(string supplierId)
    {
       List<ProductServiceContract> productServiceContract = new List<ProductServiceContract>();
-      var productListOfSupplierId= productRepository.GetProductListOfSupplierId(supplierId);
+      var productListOfSupplierId= _productRepository.GetProductListOfSupplierId(supplierId);
       if(!productListOfSupplierId.Any())
       {
         return NotFound($"SupplierId: {supplierId} Is Not Found.");
       }
       foreach(var product in productListOfSupplierId)
-          productServiceContract.Add(Convert(product));   
+         productServiceContract.Add(_mapper.Map<ProductServiceContract>(product));   
       return Ok(productServiceContract);
    } 
 
@@ -72,20 +81,19 @@ public class EStoreMaintanance : ControllerBase
    {
       if(ModelState.IsValid)
       {
-        var product = ConvertProduct(productServiceContract);
-        productRepository.AddProduct(product);
+        var product = _mapper.Map<Product>(productServiceContract);
+        _productRepository.AddProduct(product);
         return Ok();
       }
       return BadRequest();
    }
-
    [HttpPut]
    public IActionResult UpdateProduct(ProductServiceContract productServiceContract)
    {
       if(productServiceContract.Id > 0 && productServiceContract.ProductName == null && productServiceContract.ProductName == "" && productServiceContract.ProductType == null && productServiceContract.ProductType =="" && productServiceContract.Price >0 && productServiceContract.SupplierId == null && productServiceContract.SupplierId=="" && productServiceContract.UnitsinStock>0 && productServiceContract.UnitsonOrder>0 && productServiceContract.QuantityPerUnit>0)
       {
-         var product = ConvertProduct(productServiceContract);
-         productRepository.UpdateProduct(product);
+         var product = _mapper.Map<Product>(productServiceContract);
+         _productRepository.UpdateProduct(product);
          return Ok();
       }
       return BadRequest();
@@ -95,7 +103,7 @@ public class EStoreMaintanance : ControllerBase
    {
      if(productId > 0) 
      {
-      productRepository.DeleteProduct(productId);
+      _productRepository.DeleteProduct(productId);
       return Ok();
      }
      return BadRequest();
